cb475b0496d97c2dabb0425619f897cc79d6f3bd
Ivan Kataitsev <ivan.kataitsev@gmail.com>
2015-08-08 13:45:10 +0300

    Render article body with markdown

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'letter_opener', group: :development
 
+gem 'redcarpet'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,10 +3,18 @@ class Article < ActiveRecord::Base
 
   has_many :comments
 
+  def rendered_body
+    markdown.render body
+  end
+
   private
 
   def notify_subscribers
     logger.info "notify_subscribers"
     Subscription.notify_all self
+  end
+
+  def markdown
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
   end
 end
