884e2746418cb6b38c44965c8da5ddb68d7aa22c
Ivan Kataitsev <ivan.kataitsev@gmail.com>
2015-08-08 18:43:17 +0300

    Add seeds

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Simple blogging app with subscribers.
 0. Change user role in ```config/database.yml``` to your dev user.
 1. ```script/setup```
 2. ```script/server```
+3. ```bin/rake db:seed```
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create!(
+  title: "Walker Emerges as Leading Candidate to Run Enterprise Rent-a-Car Branch",
+  body: "Based on his performance in Thursday night’s debate, Wisconsin Governor Scott Walker has emerged as a leading candidate to manage an Enterprise Rent-a-Car branch, Enterprise has confirmed.
+
+Tracy Klugian, a human-resources director for the rental-car giant, said that he did not know what to expect before the debate, but after watching Walker in action he felt confident that the Wisconsinite had “what it takes to succeed at Enterprise.”
+
+“He didn’t get too many moments in the spotlight, but, just from what I saw, I would feel comfortable putting him behind one of our rental counters,” he said.
+
+He indicated that the company would be contacting Walker in the days ahead to discuss a possible future with Enterprise.
+
+“Just to be clear, no promises will be made,” he said. “Best-case scenario, Scott works hard and becomes the manager of one of our branches. For starters, we’d like to train him to be an assistant manager and see how he does with that.”
+
+Enterprise’s faith in Walker appears to be echoed by a new poll, which shows that thirty-one per cent of voters believe he is qualified to assistant-manage a rental-car branch."
+)
